d07b440679b25249616adcd3006c0cacac69fce3
Doronin Stanislav <mugisbrows@gmail.com>
2021-10-15 00:22:34 +0300

    stop on error, unhardcode versions and arch

--- build-and-install.sh
+++ build-and-install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 SCRIPT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+set -e
 cd "$SCRIPT_DIR/python-ply"
 makepkg --syncdeps --noconfirm
-pacman -U --noconfirm python-ply-3.11-1-x86_64.pkg.tar.zst
+pacman -U --noconfirm python-ply-*.pkg.tar.zst
 cd "$SCRIPT_DIR/python-pycparser"
 makepkg --syncdeps --noconfirm
-pacman -U --noconfirm python-pycparser-2.20-1-x86_64.pkg.tar.zst
+pacman -U --noconfirm python-pycparser-*.pkg.tar.zst
 cd "$SCRIPT_DIR/python-cffi"
 makepkg --syncdeps --noconfirm
-pacman -U --noconfirm python-cffi-1.14.5-1-x86_64.pkg.tar.zst
+pacman -U --noconfirm python-cffi-*.pkg.tar.zst
 cd "$SCRIPT_DIR/python-idna"
 makepkg --syncdeps --noconfirm
-pacman -U --noconfirm python-idna-3.2-1-x86_64.pkg.tar.zst
+pacman -U --noconfirm python-idna-*.pkg.tar.zst
 cd "$SCRIPT_DIR/python-cryptography"
 makepkg --nocheck --syncdeps --noconfirm
-pacman -U --noconfirm python-cryptography-3.4.7-1-x86_64.pkg.tar.zst
+pacman -U --noconfirm python-cryptography-*.pkg.tar.zst
 cd "$SCRIPT_DIR"
+set +e
